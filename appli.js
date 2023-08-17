1a86e119ac1dfa85145e9348cb2e9653a2abb0cf
camer <128632619+judicael10@users.noreply.github.com>
2023-08-17 23:35:11 +0100

    Add files via upload

---
+++ appli.js
@@ -0,0 +1,21 @@
+function show(){
+    document.querySelector('.sou-menue').classList.toggle('active-sous-menue')
+    document.queryselector('.navigation').classList.toggle('open')
+}
+
+
+
+function show2(){
+    document.querySelector('.navigation').classList.toggle('open')
+    document.querySelector('.oliver').classList.toggle('bres')
+}
+
+function show3(){
+    document.querySelector('.chiox').classList.toggle('open2')
+    document.querySelector('body').classList.toggle('bodyy')
+    document.querySelector('.navigation').classList.toggle('bodyy')
+}
+
+function show4(){
+    document.querySelector('.chiox').classList.toggle('clause')
+}
